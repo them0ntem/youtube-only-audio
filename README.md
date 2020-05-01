5369097de7bfecf4f2c673738274e38d86ff6081
Manthan Sharma <manthansharma@hotmail.com>
2020-05-02 04:05:31 +0530

    reformat code and update readme

--- README.md
+++ README.md
@@ -1,12 +1,16 @@
 Youtube Audio 
 =============
 
-Extension for turning off video on youtube. 
+Extension for turning off video on youtube. Fork of (https://github.com/animeshkundu/youtube-audio)
 
-I mostly listen to songs a lot on youtube while working. The video playing in the background just heats up my laptop, eats up my battery and data / bandwidth. Youtube doesn't provide this functionality natively.
+I mostly listen to songs a lot on youtube while working. The video playing in the background just heats up my laptop, 
+eats up my battery and data / bandwidth. Youtube doesn't provide this functionality natively.
 
 Disable video for youtube songs which saves battery life and bandwidth / data.
 
-Avaliable on firefox - [youtube-audio](https://addons.mozilla.org/en-US/firefox/addon/youtube-audio/?src=search)
+### New Features Added:
+- Remember current time-stamp of video on reload.
+- Don't require data permission for all tabs.
 
-I don't have a safari or chrome developer account. Anyone willing to help, please PM me. :)
+
+Available on firefox - [Youtube Audio by @themontem](https://addons.mozilla.org/en-US/firefox/addon/youtube-only-audio/)
--- js/global.js
+++ js/global.js
@@ -1,119 +1,122 @@
 const tabIds = new Set();
 
 function removeURLParameters(url, parameters) {
-  parameters.forEach(function(parameter) {
-    var urlparts = url.split('?');
-    if (urlparts.length >= 2) {
-      var prefix = encodeURIComponent(parameter) + '=';
-      var pars = urlparts[1].split(/[&;]/g);
+    parameters.forEach(function (parameter) {
+        let urlParts = url.split('?');
+        if (urlParts.length >= 2) {
+            let prefix = encodeURIComponent(parameter) + '=';
+            let pars = urlParts[1].split(/[&;]/g);
 
-      for (var i = pars.length; i-- > 0;) {
-        if (pars[i].lastIndexOf(prefix, 0) !== -1) {
-          pars.splice(i, 1);
+            for (let i = pars.length; i-- > 0;) {
+                if (pars[i].lastIndexOf(prefix, 0) !== -1) {
+                    pars.splice(i, 1);
+                }
+            }
+
+            url = urlParts[0] + '?' + pars.join('&');
         }
-      }
-
-      url = urlparts[0] + '?' + pars.join('&');
-    }
-  });
-  return url;
+    });
+    return url;
 }
 
 function reloadTab() {
-  for (const tabId of tabIds) {
-    chrome.tabs.get(tabId, function(tab) {
-      if (tab.active) {
-        chrome.tabs.sendMessage(tabId, {action : "curr_time"}, (resp) => {
-          var url = removeURLParameters(tab.url, [ 't' ])
-          url = url + "&t=" + resp.time;
-          var urlUpdate = chrome.tabs.update(
-              tabId,
-              {url : url},
-          );
-          urlUpdate.then(
-              (tab) => {},
-              (error) => {consle.error(`Tab update failed. ${error}`)},
-          );
+    for (const tabId of tabIds) {
+        chrome.tabs.get(tabId, function (tab) {
+            if (tab.active) {
+                chrome.tabs.sendMessage(tabId, {action: "curr_time"}, (resp) => {
+                    let url = removeURLParameters(tab.url, ['t'])
+                    url = url + "&t=" + resp.time;
+                    let urlUpdate = chrome.tabs.update(
+                        tabId,
+                        {url: url},
+                    );
+                    urlUpdate.then(
+                        () => {
+                        },
+                        (error) => {
+                            console.error(`Tab update failed. ${error}`)
+                        },
+                    );
 
-          chrome.tabs.reload(tabId);
+                    chrome.tabs.reload(tabId);
+                });
+            }
         });
-
-        return;
-      }
-    });
-  }
+    }
 }
 
 function processRequest(details) {
-  if (!tabIds.has(details.tabId)) {
-    return;
-  }
+    if (!tabIds.has(details.tabId)) {
+        return;
+    }
 
-  if (details.url.indexOf('mime=audio') !== -1 &&
-      !details.url.includes('live=1')) {
-    var parametersToBeRemoved = [ 'range', 'rn', 'rbuf' ];
-    var audioURL = removeURLParameters(details.url, parametersToBeRemoved);
-    chrome.tabs.sendMessage(details.tabId, {url : audioURL});
-  }
+    if (details.url.indexOf('mime=audio') !== -1 &&
+        !details.url.includes('live=1')) {
+        let parametersToBeRemoved = ['range', 'rn', 'rbuf'];
+        let audioURL = removeURLParameters(details.url, parametersToBeRemoved);
+        chrome.tabs.sendMessage(details.tabId, {url: audioURL});
+    }
 }
 
 function enableExtension() {
-  chrome.browserAction.setIcon(
-      {path : {128 : "img/icon128.png", 38 : "img/icon38.png"}});
-  chrome.webRequest.onBeforeRequest.addListener(
-      processRequest, {urls : [ "<all_urls>" ]}, [ "blocking" ]);
+    chrome.browserAction.setIcon(
+        {path: {128: "img/icon128.png", 38: "img/icon38.png"}});
+    chrome.webRequest.onBeforeRequest.addListener(
+        processRequest, {urls: ["<all_urls>"]}, ["blocking"]);
 }
 
 function disableExtension() {
-  chrome.browserAction.setIcon({
-    path : {
-      38 : "img/disabled_icon38.png",
-    }
-  });
-  chrome.webRequest.onBeforeRequest.removeListener(processRequest);
+    chrome.browserAction.setIcon({
+        path: {
+            38: "img/disabled_icon38.png",
+        }
+    });
+    chrome.webRequest.onBeforeRequest.removeListener(processRequest);
 }
 
 function saveSettings(currentState) {
-  chrome.storage.local.set({'youtube_audio_state' : currentState});
+    chrome.storage.local.set({'youtube_audio_state': currentState});
 }
 
-chrome.browserAction.onClicked.addListener(function() {
-  chrome.storage.local.get('youtube_audio_state', function(values) {
-    var currentState = values.youtube_audio_state;
-    var newState = !currentState;
+chrome.browserAction.onClicked.addListener(function () {
+    chrome.storage.local.get('youtube_audio_state', function (values) {
+        let currentState = values.youtube_audio_state;
+        let newState = !currentState;
 
-    if (newState) {
-      enableExtension();
-    } else {
-      disableExtension();
-    }
+        if (newState) {
+            enableExtension();
+        } else {
+            disableExtension();
+        }
 
-    saveSettings(newState);
-    reloadTab();
-  });
+        saveSettings(newState);
+        reloadTab();
+    });
 });
 
-chrome.storage.local.get('youtube_audio_state', function(values) {
-  var currentState = values.youtube_audio_state;
-  if (typeof currentState === "undefined") {
-    currentState = true;
-    saveSettings(currentState);
-  }
+chrome.storage.local.get('youtube_audio_state', function (values) {
+    let currentState = values.youtube_audio_state;
+    if (typeof currentState === "undefined") {
+        currentState = true;
+        saveSettings(currentState);
+    }
 
-  if (currentState) {
-    enableExtension();
-  } else {
-    disableExtension();
-  }
+    if (currentState) {
+        enableExtension();
+    } else {
+        disableExtension();
+    }
 });
 
 chrome.runtime.onMessage.addListener(
     (message, sender) => {
-      console.log(message, sender);
-      tabIds.add(sender.tab.id);
+        console.log(message, sender);
+        tabIds.add(sender.tab.id);
     },
 );
 
 chrome.tabs.onRemoved.addListener(
-    (tabId) => { tabIds.delete(tabId); },
+    (tabId) => {
+        tabIds.delete(tabId);
+    },
 );
